9e04a772c094b84912d26537c1d506c8dffe34f1
Coen Stange <coen.stange@hotmail.com>
2017-10-14 16:25:11 +0200

    + add empty data service

---
+++ dataservice/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+// Constants
+const PORT = 1984;
+
+// App
+const app = express();
+app.get('/', function (req, res) {
+    res.send('Test: Hello world from Web Facade!');
+});
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
