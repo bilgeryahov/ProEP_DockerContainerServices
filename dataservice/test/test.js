5898c9f22d1468d52a9e815618cb2d39099fe52b
Coen Stange <coen.stange@hotmail.com>
2017-10-15 14:06:59 +0200

    + make other graphql tests with promises

--- dataservice/test/test.js
+++ dataservice/test/test.js
@@ -11,46 +11,62 @@ const assert = require('assert');
 describe('Graphql', () => {
   describe('basic', () => {
     it(
+      'Succeed a promise',
+      () => Promise.resolve(),
+    );
+
+    it(
+      'Failed promise catch',
+      () =>
+        Promise.reject(Error('Some error'))
+          .then(() => Promise.reject(Error('Previous error should have failed')))
+          .catch(error => (error.message === 'Some error' ? Promise.resolve() : Promise.reject(error))),
+    );
+
+    it(
       'Can run graphql query',
       () =>
         graphql(schema, '{ hello }', root)
-          .then(
-            response =>
-              new Promise((resolve, reject) => {
-                if (response.data.hello === 'Hello world!') {
-                  resolve();
-                } else {
-                  reject(Error(`Expected response hello world but got ${JSON.stringify(response)}`));
-                }
-              }),
-            error => Promise.reject(error),
-          ),
+          .then(response =>
+            new Promise((resolve, reject) => {
+              if (response.data.hello === 'Hello world!') {
+                resolve();
+              } else {
+                reject(Error(`Expected response hello world but got ${JSON.stringify(response)}`));
+              }
+            })),
     );
   });
 
   describe('users', () => {
-    it('Create a user', (done) => {
-      graphql(schema, '{ registerUser { "name": "testuser", "password": "testpassword" } }', root).then((response) => {
-        assert.true(response.data.succeed);
-        return graphql(schema, '{ registerUser { "name": "testuser", "password": "testpassword" } }', root);
-      }).then((response) => {
-        assert.false(response.data.succeed);
-        done();
-      });
-    });
+    it(
+      'Create a user',
+      () =>
+        graphql(schema, '{ registerUser { "name": "testuser", "password": "testpassword" } }', root)
+          .then((response) => {
+            if (!response.data.registerUser.succeed) {
+              return Promise.reject(Error('Couldn\'t create first user'));
+            }
+            return graphql(schema, '{ registerUser { "name": "testuser", "password": "testpassword" } }', root);
+          }).then(response =>
+            new Promise((resolve, reject) => {
+              if (!response.data.registerUser.succeed) {
+                resolve();
+              } else {
+                reject(Error('User should already be registered, user could be made again'));
+              }
+            })),
+    );
 
-    it('Login a user', (done) => {
-      graphql(schema, '{ users(name: "testuser", password: "testpassword") { id } }', root).then((response) => {
-        assert.integer(response.data.users.id);
-        done();
-      });
-    });
+    it('Login a user', () =>
+      graphql(schema, '{ users(name: "testuser", password: "testpassword") { id } }', root)
+        .then((response) => {
+          assert.integer(response.data.users.id);
+        }));
 
-    it('Login a user wrong password', (done) => {
+    it('Login a user wrong password', () =>
       graphql(schema, '{ users(name: "testuser", password: "testpassword") { id } }', root).then((response) => {
         assert.true(response.data.users == null);
-        done();
-      });
-    });
+      }));
   });
 });
