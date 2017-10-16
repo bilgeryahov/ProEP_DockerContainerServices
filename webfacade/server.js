11122fc992d7bb385edcc97ec74628a215898030
Coen Stange <coen.stange@hotmail.com>
2017-10-16 20:46:21 +0200

    + make web facade work

--- webfacade/server.js
+++ webfacade/server.js
@@ -1,13 +1,19 @@
-const express = require('express');
+import { newConnection } from './src';
+
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 // Constants
 const PORT = 9090;
 
 // App
-const app = express();
 app.get('/', (req, res) => {
   res.send('Test: Hello world from Web Facade!');
 });
 
-app.listen(PORT);
-console.log(`Running on http://localhost:${PORT}`);
+io.on('connection', newConnection);
+
+http.listen(PORT, () => {
+  console.log(`Running on http://localhost:${PORT}`);
+});
--- webfacade/test/test.js
+++ webfacade/test/test.js
@@ -2,32 +2,43 @@
 // const assert = require('assert');
 const socketioClient = require('socket.io-client');
 
-const client = socketioClient('http://127.0.0.1:9090/', { forceNew: true });
-
-describe('Socketclient', () => {
-  before(() => new Promise((resolve, reject) => {
+const client = socketioClient.connect('http://127.0.0.1:9090', { forceNew: true, transports: ['websocket'] });
+const connectionPromise =
+  new Promise((resolve, reject) => {
     client.on('connect', () => {
+      console.log('Connected');
       resolve();
     });
 
     client.on('disconnect', () => {
+      console.log('Disconnected');
+      reject(Error('Disconnect server'));
+    });
+
+    client.on('connect_error', () => {
+      console.log('Error on connection');
       reject(Error('Could not connect to server'));
     });
-  }));
-  describe('#indexOf()', () => {
+  });
+
+describe('Socketclient', () => {
+  before(() => connectionPromise);
+  describe('User', () => {
     it('should register and login', () => {
       const registerPromise = new Promise((resolve) => {
-        client.on('register', resolve);
+        client.once('register', resolve);
       });
       const loginPromise = new Promise((resolve) => {
-        client.on('login', resolve);
+        client.once('login', resolve);
       });
       client.emit('register', { name: 'steve', email: 'steve@me.com', pass: 'mypass' });
       return registerPromise
         .then((data) => {
           if (!data.succeed) {
+            console.error(data);
             return Promise.reject(Error('Register failed'));
           }
+          console.log('Registered');
           client.emit('login', { name: 'steve', pass: 'mypass' });
           return loginPromise;
         })
