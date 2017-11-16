a8bc6bd3c499d7f6051fb1ea318c02d9dbb6f33e
Coen Stange <coen.stange@hotmail.com>
2017-11-16 20:47:08 +0100

    + add stream container

---
+++ stream/src/api.js
@@ -0,0 +1,31 @@
+import { buildSchema } from 'graphql';
+
+const amqp = require('amqp');
+
+// RabbitMQ stuff
+const connection = amqp.createConnection({ host: 'amqp://user:user@rabbit:5672' });
+// add this for better debuging
+connection.on('error', (e) => {
+  console.log('Error from amqp: ', e);
+});
+
+// Join a queue
+new Promise(resolve => connection.on('ready', resolve))
+  .then(() => new Promise(resolve => connection.queue('phonemeta', resolve)));
+
+export const schema = buildSchema(`
+type Query {
+  hello: String
+  sendPhoneMeta(data: String!): Boolean!
+}
+`);
+
+export const root =
+{
+  hello: () => 'Hello world from stream!',
+  sendPhoneMeta: (data) => {
+    connection.publish('phonemeta', JSON.stringify(data));
+    return true;
+  },
+};
+
