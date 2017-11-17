8f60c9fcd0220886ad21a4ede8d8a049f78708a3
Coen Stange <coenstange@me.com>
2017-11-17 13:36:49 +0100

    Revert "Revert "Add location broadcasting""

--- phonefacade/server.js
+++ phonefacade/server.js
@@ -1,19 +1,18 @@
-import { newConnection, rabbitToSocket } from './src';
+import { newConnection } from './src';
 
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 // Constants
-const PORT = 9090;
+const PORT = 1903;
 
 // App
 app.get('/', (req, res) => {
-  res.send('Test: Hello world from Web Facade!');
+  res.send('Test: Hello world from Phone Facade!');
 });
 
 io.on('connection', newConnection);
-rabbitToSocket(io);
 
 http.listen(PORT, () => {
   console.log(`Running on http://localhost:${PORT}`);
