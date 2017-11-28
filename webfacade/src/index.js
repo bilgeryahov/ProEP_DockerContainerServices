164281c8ba96f00a655301e35ad305fa6539fdfa
Coen Stange <coen.stange@hotmail.com>
2017-11-28 15:58:36 +0100

    Revert "Start implementation of user subscription."
    This reverts commit a7833d18fb4fb9278ef15c2bf0a16c09b9da45d0.

--- webfacade/src/index.js
+++ webfacade/src/index.js
@@ -1,13 +1,10 @@
 import { GraphQLClient } from 'graphql-request';
 
-const rp = require('request-promise');
-
 const client = new GraphQLClient('http://authentication:9000/graphql', { headers: {} });
 const amqp = require('amqp');
 
 // RabbitMQ stuff
 const connection = amqp.createConnection({ host: 'amqp://user:user@rabbit:5672' });
-const PAYMENT_URL = 'http://40.68.124.79:1997/pay';
 
 // Join a queue
 const queuePromise = new Promise(resolve => connection.on('ready', resolve))
@@ -21,7 +18,6 @@ export const newConnection = (socket) => {
   console.log('New connection');
   socket.join('singleroom'); // refactor later to work on multiple streams
   let userId = null;
-  let userName = null;
 
   socket.on('login', (msg) => {
     console.log(`Login: ${JSON.stringify(msg)}`);
@@ -32,7 +28,6 @@ export const newConnection = (socket) => {
             socket.emit('login', { succeed: false, message: 'Already logged in' });
           } else if (x.user != null && Number.isInteger(x.user.id)) {
             userId = x.user.id;
-            userName = msg.name;
             socket.emit('login', { succeed: true, message: '', userData: x.user });
           } else {
             socket.emit('login', { succeed: false, message: 'Wrong username or password' });
@@ -61,34 +56,6 @@ export const newConnection = (socket) => {
     } else {
       socket.emit('register', { succeed: false, message: 'Give both username, email and password' });
     }
-  });
-
-  socket.on('subscribeUser', (name) => {
-    client.request('query subscribeUser($name: String!){ subscribeUser (name: $name) { subscribed } }', name)
-      .then((x) => {
-        if (x !== 0) {
-          socket.emit('subscribeUser', { succeed: false, message: 'Cannot subscribe!' });
-        } else {
-          rp({
-            uri: PAYMENT_URL,
-            method: 'POST',
-            resolveWithFullResponse: true,
-          }).then((response) => {
-            if (response.status === 201) {
-              // Subscribe user.
-            } else {
-              socket.emit('subscribeUser', { succeed: false, message: 'Cannot subscribe!' });
-            }
-          })
-            .catch((err) => {
-              socket.emit('subscribeUser', { succeed: false, message: `Error ${err}` });
-            });
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-        socket.emit('subscribeUser', { succeed: false, message: `Error ${err}` });
-      });
   });
 };
 
