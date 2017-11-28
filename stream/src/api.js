91569f739cb59319cfdb379b4f783abdf5505c17
bloslo <jheru@abv.bg>
2017-11-28 15:57:16 +0100

    Add procedure to generate uuid for stream.

--- stream/src/api.js
+++ stream/src/api.js
@@ -1,12 +1,24 @@
 import { buildSchema } from 'graphql';
 
 const amqp = require('amqp');
+const uuidv4 = require('uuid/v4');
+const redis = require('redis');
+
+const client = redis.createClient('redis');
 
 // RabbitMQ stuff
 const connection = amqp.createConnection({ host: 'amqp://user:user@rabbit:5672' });
 // add this for better debuging
 connection.on('error', (e) => {
   console.log('Error from amqp: ', e);
+});
+
+// Redis stuff.
+client.on('error', (err) => {
+  console.log(`Error from redis: ${err}`);
+});
+client.on('connect', () => {
+  console.log('Connected to Redis');
 });
 
 // Join a queue
@@ -22,6 +34,7 @@ export const schema = buildSchema(`
 type Query {
   hello: String
   sendPhoneMeta(data: String!): Boolean!
+  initStream(username: String!): String!
 }
 `);
 
@@ -32,6 +45,12 @@ export const root =
     // console.log(`publish ${JSON.stringify(data)}`);
     connection.publish('phonemeta', JSON.parse(data.data));
     return true;
+  },
+  initStream: (username) => {
+    const uuid = uuidv4();
+    client.hmset('streamers', { [uuid]: username });
+
+    return uuid;
   },
 };
 
--- stream/test/test.js
+++ stream/test/test.js
@@ -1,1 +1,28 @@
 /* eslint-env node, mocha */
+import { graphql } from 'graphql';
+
+import { schema, root } from './../src/api';
+
+// const assert = require('assert');
+
+describe('stream service', () => {
+  describe('initStream', () => {
+    it('can run graphql query', () =>
+      graphql(schema, '{ hello }', root)
+        .then(response =>
+          new Promise((resolve, reject) => {
+            if (response.data.hello === 'Hello world from stream!') {
+              resolve();
+            } else {
+              reject(Error(`Expected response hello world but got ${JSON.stringify(response)}`));
+            }
+          })));
+
+    it('should initialize stream', () =>
+      graphql(schema, '{ initStream (username: "testuser") }', root)
+        .then((response) => {
+          console.log('Response: ', response);
+          // assert.ok(typeof response.data.initStream === 'string');
+        }));
+  });
+});
