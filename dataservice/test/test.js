1664976009af3bb8a17853899da6d912cdf31c08
Coen Stange <coen.stange@hotmail.com>
2017-12-03 00:28:44 +0100

    + add functions to the facades

--- dataservice/test/test.js
+++ dataservice/test/test.js
@@ -20,7 +20,7 @@ describe('Graphql', () => {
       .then(() =>
         model.User.destroy({ // Remove testdata before running
           where: {
-            $or: [{ username: 'testuser' }, { username: 'george' }, { username: 'steve' }, { username: 'georgesoroz' }],
+            $or: [{ username: 'testuser' }, { username: 'george' }, { username: 'steve' }, { username: 'georgesoroz' }, { username: 'asana' }],
           },
         }))
       .catch((err) => {
