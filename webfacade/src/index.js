06a81870afdf97fc4fb1461cf2d7de6e5d44d6e1
bloslo <jheru@abv.bg>
2018-01-05 14:04:16 +0100

    Emit chat event to all, including sender.

--- webfacade/src/index.js
+++ webfacade/src/index.js
@@ -117,7 +117,7 @@ export const newConnection = (socket) => {
   });
 
   socket.on('chatMessage', (message) => {
-    socket.emit('newMessage', { id: userId, user: username, msg: message });
+    io.emit('newMessage', { id: userId, user: username, msg: message });
   });
 };
 
