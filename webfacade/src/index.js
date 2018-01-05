2efc45d8bc451fd58a28efcb4c4a640411577c59
bloslo <jheru@abv.bg>
2018-01-05 15:12:04 +0100

    Sending chat message to all clients in a room.

--- webfacade/src/index.js
+++ webfacade/src/index.js
@@ -117,7 +117,7 @@ export const newConnection = (socket) => {
   });
 
   socket.on('chatMessage', (message) => {
-    socket.broadcast.emit('newMessage', { id: userId, user: username, msg: message });
+    socket.to(room).emit('newMessage', { id: userId, user: username, msg: message });
   });
 };
 
