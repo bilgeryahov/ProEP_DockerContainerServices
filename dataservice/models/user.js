db14dc99f4d6057c0ee5941c3df48919f43d4314
Coen Stange <coen.stange@hotmail.com>
2018-01-07 19:15:46 +0100

    + fix database model to support many to many subscription

--- dataservice/models/user.js
+++ dataservice/models/user.js
@@ -1,11 +1,12 @@
 module.exports = (sequelize, DataTypes) => {
+  const userSubscription = sequelize.define('UserSubscription', {
+  });
   const User = sequelize.define('User', {
     username: DataTypes.STRING,
     password: DataTypes.STRING,
     email: DataTypes.STRING,
   });
 
-  User.hasMany(User, { as: 'SubscribeTo' });
-
+  User.belongsToMany(User, { as: 'SubscribeTo', through: { model: userSubscription }, foreignKey: 'subscriberid' });
   return User;
 };
